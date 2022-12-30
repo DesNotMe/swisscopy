4e1c93e37728e5bea497e7b81aef8dcdcd8deb6c
dexdreamin <dexterkhoowc@gmail.com>
2022-12-30 12:31:39 +0800

    modified:   website/forms.py 	modified:   website/models.py

--- website/forms.py
+++ website/forms.py
@@ -40,6 +40,47 @@ class RegisterForm(FlaskForm):
     password2 = PasswordField(label='Confirm Password:', validators=[EqualTo('password1'), DataRequired()])
     submit = SubmitField(label='Create Account')
 
+class RegisterRetailAccountForm(FlaskForm):
+    # The Validator library allows you to create certain functions
+    # with specific usernames which lets the flaskform class
+    # do certain stuff for you.
+    # The flask form will search for all function names
+    # starting with the prefix validate and check if there is even a field
+    # with that given name. Once everything is checked out
+    # flaskform knows it needs to validate that username
+    def validate_username(self, username_to_check):
+        user = User.query.filter_by(username=username_to_check.data).first()
+        # if this returns an object
+        if user:
+            # checks if user is not 'None'
+            # so ya if it returns an object it means this is
+            # an existing user created before which raises this error
+            raise ValidationError('Username already exist! Please try a different username.')
+
+    def validate_email_address(self, email_address_to_check):
+        email_address = User.query.filter_by(email_address=email_address_to_check.data).first()
+        if email_address:
+            # check if email_address is not 'None'.
+            raise ValidationError("Email Address already exist. Please try a different email address.")
+
+    # User.query.filter_by(username = username_to_check) will return an object
+    # .first() is used to access the first object
+
+    username = StringField(label='User Name:', validators=[Length(min=2, max=30), DataRequired()])
+    email_address = StringField(label='Email Address:', validators=[Email(), DataRequired()])
+    password1 = PasswordField(label='Password:', validators=[Length(min=6), DataRequired()])
+    password2 = PasswordField(label='Confirm Password:', validators=[EqualTo('password1'), DataRequired()])
+    submit = SubmitField(label='Create Account')
+
+class RegisterRetailerForm(FlaskForm):
+    shop = StringField(label='Name of retail shop: ', validators=DataRequired())
+    postal_code = IntegerField(label='Postal code: ', validators=[Length(max=6), DataRequired()])
+    unit_number = StringField(label='Unit-number: ', validators=DataRequired())
+    address = StringField(label="Address: ", validators=[DataRequired()])
+    office_no = StringField(label="Office number: ", validators=DataRequired())
+    email_address = EmailField(label='Email Address:', validators=[Email(), DataRequired()])
+    date_registered = DateField(label="Date registered: ", validators=DataRequired())
+    submit = SubmitField(label="Submit")
 
 class LoginForm(FlaskForm):
     username = StringField(label='User Name:', validators=[DataRequired()])
--- website/models.py
+++ website/models.py
@@ -86,6 +86,127 @@ class User(db.Model, UserMixin):
         otp = ''.join(random.choice(chars) for _ in range(size))
         return otp
 
+class RetailAccount(db.Model):
+    id = db.Column(db.Integer(), primary_key=True)
+    admin = db.Column(db.Integer())
+    # the id unique to each user so that flask can identify each individual user
+    username = db.Column(db.String(length=30), nullable=False, unique=True)
+    email_address = db.Column(db.String(length=50), nullable=False, unique=True)
+    password_hash = db.Column(db.String(length=60), nullable=False, unique=True)
+    # the mostly used hashing algorithm that flask allow us to use
+    # will always convert the passwords to being 60 characters
+    # thats why length is set to 60
+    date = db.Column(db.String(), default=datetime.now().strftime("%d/%m/%Y"))
+    budget = db.Column(db.Integer(), nullable=False, default=10000)
+    gender = db.Column(db.String(), nullable=False, default='Rather not say')
+    messages = db.Column(db.Integer(), nullable=False, default=0)
+    # items = db.relationship('Item', backref='owned_user', lazy=True)
+    # relationship is to make it so some items have some relationship to
+    # the user.
+    shoppingCartCount = db.Column(db.Integer(), nullable=False, default=0)
+    profits = db.Column(db.Integer(), nullable=False, default=0)
+    spending = db.Column(db.Integer(), nullable=False, default=0)
+
+    # Account status (Adds 'Disabled' or 'Enabled' status column to User Database)
+    status = db.Column(db.Integer(), nullable=False, default='Enabled')
+
+    @property
+    def prettier_budget(self):
+        # to confirm this is to add a , in the budget
+        # e.g 1,000 or 100,000
+        # this relies on logic alone not special class names
+        # affecting this function
+        # if len(str(self.budget)) >= 4:
+        #     return f'${str(self.budget)[:-3]},{str(self.budget)[-3:]}'
+        # else:
+        return f"${self.budget:,.2f}"
+
+    @property
+    def password(self):
+        return self.password
+
+    # return password
+    @password.setter
+    def password(self, plain_text_password):
+        self.password_hash = bcrypt.generate_password_hash(plain_text_password).decode('utf-8')
+
+    # hashes the password entered by users creating new accounts
+
+    def check_password_correction(self, attempted_password):
+        return bcrypt.check_password_hash(self.password_hash, attempted_password)
+        # return true or false
+
+    def can_deposit(self, deposit_to_check):
+        return deposit_to_check > 0
+
+    def account_availability(self, status):
+        if status == "Disabled":
+            return 0
+
+    # Password Reset Function
+    def password_otp(self, size=8, chars=string.ascii_uppercase + string.digits):
+        otp = ''.join(random.choice(chars) for _ in range(size))
+        return otp
+
+    # Make sure otp cannot be reused again.
+    def scramble_otp(self, size=128, chars=string.ascii_uppercase + string.digits):
+        otp = ''.join(random.choice(chars) for _ in range(size))
+        return otp
+    
+
+class Retail:
+    count_id = 0
+    def __init__(self, location, postal_code, unit_number, address, office_no, email_address, date_registered):
+        self.__location = location
+        self.__postal_code = postal_code
+        self.__unit_number = unit_number
+        self.__address = address
+        self.__office_no = office_no
+        self.__email_address = email_address
+        self.__date_registered = date_registered
+
+    def get_location(self):
+        return self.__location
+
+    def get_postal_code(self):
+        return self.__postal_code
+
+    def get_unit_number(self):
+        return self.__unit_number
+
+    def get_email_address(self):
+        return self.__email_address
+
+    def get_address(self):
+        return self.__address
+
+    def get_office_no(self):
+        return self.__office_no
+
+    def get_date_registered(self):
+        return self.__date_registered
+
+    def set_location(self, location):
+        self.__location = location
+
+    def set_postal_code(self, postal_code):
+        self.__postal_code = postal_code
+
+    def set_unit_number(self, unit_number):
+        self.__unit_number = unit_number
+
+    def set_email_address(self, email_address):
+        self.__email_address = email_address
+
+    def set_address(self, address):
+        self.__address = address
+
+    def set_office_no(self, office_no):
+        self.__office_no = office_no
+
+    def set_date_registered(self, date):
+        self.__date_registered = date
+
 
 class Img(db.Model):
     id = db.Column(db.Integer, primary_key=True)
